ea6d9f1711694b76c8fb972301c7291675ebbdaa
Marcus Ziadé <marcus.ziade@ouraring.com>
2021-11-13 08:52:02 +0200

    Initial Commit

---
+++ master-of-stoicism/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  master-of-stoicism
+//
+//  Created by Marcus Ziadé on 13.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ master-of-stoicism/master_of_stoicismApp.swift
@@ -0,0 +1,17 @@
+//
+//  master_of_stoicismApp.swift
+//  master-of-stoicism
+//
+//  Created by Marcus Ziadé on 13.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct master_of_stoicismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
